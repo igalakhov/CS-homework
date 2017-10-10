99f7b338a4ff2f1caf62e490eebd8db529de88a3
Ivan Galakhov <igalakhov00@stuy.edu>
2017-10-10 18:25:12 -0400

    ORCHESTRA

--- anagramFinder/anagramFinder.java
+++ anagramFinder/anagramFinder.java
@@ -6,28 +6,57 @@ import java.io.*;
 import java.util.*;
 
 public class anagramFinder {
+  public static int[] primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101};
  public static void main(String[] args) throws IOException {
    //the word whos anagram we are trying to find
-   String toFind = "horsecart"; //anagram of the word we are trying to find
    String fileName = "words.txt"; //name of file with all the words (relative)
-   
+   long toFind = 1738749289310L; //prime representation of horsecart
+     
    File wordList = new File(fileName);
    
    ArrayList<String> lines = getLines(wordList); //all the words in the test document in an arrayList
-   ArrayList<ArrayList<Byte>> wordNums = convertToNums(lines);
+   ArrayList<ArrayList<Integer>> wordNums = convertToNums(lines);
+   long[] encodedWords = multiplyChars(wordNums);
+   
+   ArrayList<Integer> found = new ArrayList<Integer>();
+   for (int i = 0; i < encodedWords.length; i++){
+     if(encodedWords[i] == toFind){
+       found.add(i);
+     }
+   }
+   //print out the results
+   System.out.println("Found Words:");
+   for(int i = 0; i < found.size(); i++){
+     System.out.println(lines.get(found.get(i)));
+   }
    
    }
  //converts all words into their ASCII values - 97
- public static ArrayList<ArrayList<Byte>> convertToNums(ArrayList<String> words){
-   ArrayList<ArrayList<Byte>> out = new ArrayList<ArrayList<Byte>>();
+ public static long[] multiplyChars(ArrayList<ArrayList<Integer>> words){
+   long[] out = new long[words.size()];
+   for (int i = 0; i < words.size(); i++){
+    ArrayList<Integer> curWord = words.get(i);
+    long product = 1;
+    for(int j = 0; j < curWord.size(); j++){
+     product *= primes[curWord.get(j)]; 
+    }
+    out[i] = product;
+   }
+   return out;
+ }
+ public static ArrayList<ArrayList<Integer>> convertToNums(ArrayList<String> words){
+   ArrayList<ArrayList<Integer>> out = new ArrayList<ArrayList<Integer>>();
    for(int i = 0; i < words.size(); i++){
      String curWord = words.get(i);
-     String[] splitChars = curWord.split("");
-     ArrayList<Byte> temp = new ArrayList<Byte>();
-     for(int j = 0; i < splitChars.length; j++){
-       temp.add((byte)((int) splitChars[j].charAt(0)));
+     ArrayList<Integer> tempChars = new ArrayList<Integer>();
+     for(int j = 0; j < curWord.length(); j++){
+      Character curChar = curWord.charAt(j);
+      int curAscii = (int) curChar;
+      if(curAscii >= 97 && curAscii <= 122){
+        tempChars.add((curAscii - 97));
+      }  
      }
-
+     out.add(tempChars);
    }
    return out;
  }
@@ -44,6 +73,7 @@ public class anagramFinder {
     while ((curLine = in.readLine()) != null){
       out.add(curLine);
     }
+    in.close();
   } catch (FileNotFoundException e){
     System.out.println(e);
   }
